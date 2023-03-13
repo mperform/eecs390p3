02a4c4cdabd8f076461838842fb570354ef873dd
mperform <122114676+mperform@users.noreply.github.com>
2023-03-12 20:40:47 -0400

    updating phase 3

--- core.scm
+++ core.scm
@@ -239,7 +239,7 @@
         ; (scheme-define global-env '(func x z) '(+ x (- y z)))
         ((list? (car args)) ; case 2: (define (<variable> <formals>) <body>)
           (cond 
-            ((all-symbols? (cdr (car args))) ;; checks to make sure formals are all symbols
+            ((all-symbols? (car args))) ;; checks to make sure formals are all symbols
               ; (env 'insert name (scheme-lambda formals body))
               (env 'insert (car (car args)) (scheme-lambda env (cdr (car args)) (car (cdr args))))
             )
